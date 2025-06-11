a6e201a0cf4c3cab119a7748007c1664b46dba1d
Damian A. <damian.cods.esteban@gmail.com>
2025-06-10 21:12:03 -0600

    Agregar archivos de proyecto.

---
+++ Inventario/Producto.cs
@@ -0,0 +1,80 @@
+﻿namespace Inventario
+{
+    public class Producto
+    {
+        private string codigo_Producto;
+        private string nombre_Producto;
+        private string categoria_Producto;
+        private string estado_Producto; //PROBABLEMENTE SE ELIMINE...
+        private int cantida_Producto;
+        private double precio_Producto;
+
+        private string fecha_Registro;
+        private string fecha_Vencimiento;
+
+        
+        public Producto(string Codigo_Producto, string Nombre_Producto, string Categoria_Producto, string Estado_Producto, int Cantida_Producto, double Precio_Producto, string Fecha_Registro, string Fecha_Vencimiento)
+        {
+            //Decidir si los parametros se inician con mayúscula...
+
+            this.codigo_Producto = Codigo_Producto;
+            this.nombre_Producto = Nombre_Producto;
+            this.categoria_Producto = Categoria_Producto;
+            this.estado_Producto = Estado_Producto;
+            this.cantida_Producto = Cantida_Producto;
+            this.precio_Producto = Precio_Producto;
+
+            this.fecha_Registro = Fecha_Registro;
+        }
+
+        public string Codigo_Producto
+        {
+            get { return this.codigo_Producto; }
+            set { codigo_Producto = value; }
+        }
+
+        public string Nombre_Producto
+        {
+            get { return this.nombre_Producto; }
+            set { codigo_Producto = value; }
+        }
+
+        public string Categoria_Producto
+        {
+            get { return this.categoria_Producto; }
+            set { categoria_Producto = value; }
+        }
+
+        public string Estado_Producto
+        {
+            get { return this.estado_Producto; }
+            set { estado_Producto = value; }
+        }
+
+        public int Cantida_Producto
+        {
+            get { return this.cantida_Producto; }
+            set { cantida_Producto = value; }
+        }
+
+        public double Precio_Producto
+        {
+            get { return this.precio_Producto; }
+            set { precio_Producto = value; }
+        }
+
+        public string Fecha_Registro
+        {
+            get { return this.fecha_Registro; }
+            set { fecha_Registro = value; }
+        }
+
+        public string Fecha_Vencimiento
+        {
+            get { return this.fecha_Vencimiento; }
+            set { fecha_Vencimiento = value; }
+        }
+
+
+    }
+}
---
+++ Inventario/Venta.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Capa_Entidad
+{
+    internal class Venta
+    {
+        private string id_Venta;
+        private string fecha_Venta;
+        private int cantidad_Venta;
+
+        private double precio_Unitario;
+        private double subtotal;
+        private double impuesto;
+        private double total;
+
+        public Venta(string Id_Venta, string Fecha_Venta, int Cantidad_Venta, double Precio_Unitario, double Subtotal, double Impuesto, double Total)
+        {
+            this.id_Venta = Id_Venta;
+            this.fecha_Venta = Fecha_Venta;
+            this.cantidad_Venta = Cantidad_Venta;
+
+            this.precio_Unitario = Precio_Unitario;
+            this.subtotal = Subtotal;
+            this.impuesto = Impuesto;
+            this.total = Total;
+        }
+
+        public string Id_Venta
+        {
+            get { return id_Venta; }
+            set { id_Venta = value; }
+        }
+
+        public string Fecha_Venta
+        {
+            get { return fecha_Venta; }
+            set { fecha_Venta = value; }
+        }
+
+        public int Cantidad_Venta
+        {
+            get { return cantidad_Venta;}
+            set { cantidad_Venta= value; }
+        }
+
+        public double Precio_Unitario
+        {
+            get { return precio_Unitario; }
+            set { precio_Unitario = value; }
+        }
+
+        public double Subtotal
+        {
+            get { return subtotal; }
+            set { subtotal = value; }
+        }
+
+        public double Impuesto
+        {
+            get { return impuesto; }
+            set { impuesto = value; }
+        }
+
+        public double Total
+        {
+            get { return total; }
+            set { total = value; }
+        }
+
+    }
+}
