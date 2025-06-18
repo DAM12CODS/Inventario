cd71b3fbea27d18927c18701592cc93a46ca85ad
DAM12CODS <esteban.aguilar@epn.edu.ec>
2025-06-17 21:55:42 -0600

    Update Class1.cs
    Cambios originales Clase de Datos

--- CapaDatos/Class1.cs
+++ CapaDatos/Class1.cs
@@ -53,56 +53,7 @@ namespace CapaDatos
                     ventaActual.AgregarDetalle(detalle);
                 }
             }
-
             return ventas;
-        }
-    }
-
-
-  
-}
-class Program
-{
-    static void Main(string[] args)
-    {
-        string pathfile = AppDomain.CurrentDomain.BaseDirectory + "miArchivo";
-        SLDocument oSLDocument = new SLDocument();
-        DataTable table = new DataTable();
-        //Columnas 
-        table.Columns.Add("Id", typeof(int));
-        table.Columns.Add("Nombre", typeof(string));
-        table.Columns.Add("Precio", typeof(double));
-
-        //Registro 
-        table.Rows.Add(1, "Paleta", 5.50);
-        table.Rows.Add(2, "Papas", 12);
-        table.Rows.Add(3, "Galletas", 10); 
-
-        oSLDocument.ImportDataTable(1,1,table, true);
-        oSLDocument.SaveAs(pathfile);
-
-        //Leer archivo exel 
-        int indice = 1; 
-        SLDocument miArchivo= new SLDocument(pathfile);
-
-        while (!string.IsNullOrEmpty(miArchivo.GetCellValueAsString(indice, 1))) 
-        {
-            if(indice == 1)
-            {
-                string enc1 = miArchivo.GetCellValueAsString(indice, 1);
-                string enc2 = miArchivo.GetCellValueAsString(indice, 2);
-                string enc3 = miArchivo.GetCellValueAsString(indice, 3);
-                Console.WriteLine(enc1, enc2, enc3);
-            }
-            else
-            {
-                int id = miArchivo.GetCellValueAsInt32(indice, 1);
-                string nombre = miArchivo.GetCellValueAsString(indice, 2);
-                double precio = miArchivo.GetCellValueAsDouble(indice, 3);
-                Console.WriteLine(id + "   " + nombre + "   " + precio);
-            }
-
-                indice++;
         }
     }
 }
